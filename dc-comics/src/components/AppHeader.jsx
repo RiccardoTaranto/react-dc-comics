716d3beb521a0eb6a0f6a5e256f86c3d3a8a3c6a
Riccardo <riccardo.taranto@icloud.com>
2026-04-20 08:20:47 +0200

    add navbar in header add some style in header and nav

--- dc-comics/src/components/AppHeader.jsx
+++ dc-comics/src/components/AppHeader.jsx
@@ -7,7 +7,18 @@ export default function AppHeader() {
     return (
         <header>
             <img src={AppLogo} alt="" />
-            <h1>DC-COMICS</h1>
+            <nav>
+                <a href="">CHARACTER</a>
+                <a href="">COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+            </nav>
         </header>
     )
 }
